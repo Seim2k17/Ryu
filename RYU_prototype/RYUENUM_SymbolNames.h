673d0aec34e4b45ba1e1976841486fa8d35e11ea
Seim2k12 <ryu.game2k17@gmail.com>
2017-12-10 19:08:10 +0100

    EDIT: more Keys in Struct symbolnames

--- RYU_prototype/RYUENUM_SymbolNames.h
+++ RYU_prototype/RYUENUM_SymbolNames.h
@@ -7,4 +7,9 @@ enum class ESymbolNames : uint8
 	none UMETA(DisplayName = "none"),
 	Sun	UMETA(DisplayName = "Sun"),
 	Moon UMETA(DisplayName = "Moon"),
+	Wave_1 UMETA(DisplayName ="Wave_1"),
+	Wave_2 UMETA(DisplayName = "Wave_2"),
+	Wave_3 UMETA(DisplayName = "Wave_3"),
+	Wave_4 UMETA(DisplayName = "Wave_4"),
+	Wave_5 UMETA(DisplayName = "Wave_5"),
 };
