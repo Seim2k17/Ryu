916d89adb82aa97161d4416e4b6ec3deb172f209
Simon Nguyen <sng198022@googlemail.com>
2018-01-31 13:49:13 +0100

    DEL: MaxNotes (moved to folder _sec) ADD: WalkSplineComponent for Doors etc... (incomplete)

---
+++ RYU_prototype/WalkSplineComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Runtime/Engine/Classes/Components/SplineComponent.h"
+#include "WalkSplineComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class RYU_PROTOTYPE_API UWalkSplineComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UWalkSplineComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(VisibleAnywhere)
+		USplineComponent* connectedSpline;
+		
+	
+};
